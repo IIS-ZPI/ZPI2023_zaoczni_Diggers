90bd0f569b3dc4421be5c8110bdf671aab9e3174
gruszm <mariuszgruszczynski11@gmail.com>
2023-12-10 18:25:26 +0100

    removed redundant import

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ print("Diggers; tester; Pavlucek")
 print("Developer: Mirek0206")
 print("Diggers; developer; gruszm")
 
-from abc import ABC, abstractmethod
-
 class ArithmeticDifference(ABC):
     @abstractmethod
     def Difference[T: float](self, a: T, b: T) -> T:
