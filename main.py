5e12ec6b3cc2e7265d139a7f503c4c5ec7b8fae4
Damian Mirowski <damian_mirowski@wp.pl>
2023-12-10 17:54:15 +0100

    Added ArithmeticsAdd abstract class.

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from abc import ABC, abstractclassmethod
+
 print("Diggers; DevOPs + Scrum Master; maresyp")
 print("maresyp")
 print("Diggers; tester; Pavlucek")
 print("Developer: Mirek0206")
 print("Diggers; developer; gruszm")
+
+class ArithmeticsAdd(ABC):
+    @abstractclassmethod
+    def Addition[T: float](self, a: T, b: T) -> T:
+        raise NotImplementedError
