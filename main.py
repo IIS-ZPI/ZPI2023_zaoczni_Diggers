e469fa58969623d3c3812bb7f81a0ccda5f1868f
gruszm <mariuszgruszczynski11@gmail.com>
2023-12-10 17:54:04 +0100

    Added difference abstract method

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ print("maresyp")
 print("Diggers; tester; Pavlucek")
 print("Developer: Mirek0206")
 print("Diggers; developer; gruszm")
+
+from abc import ABC, abstractmethod
+
+class ArithmeticDifference(ABC):
+    @abstractmethod
+    def Difference[T: float](self, a: T, b: T) -> T:
+        raise NotImplementedError
