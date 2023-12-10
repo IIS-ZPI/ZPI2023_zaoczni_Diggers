7e29c0c8fb29a006f7e3f63f90453e61ab925a15
maresyp <mattq.2000@icloud.com>
2023-12-10 23:39:50 +0100

    Revert "Refactor abstract class method names"
    This reverts commit 866c999aebaf23c3c94f80254d13b60bf8c55b86.

--- main.py
+++ main.py
@@ -8,20 +8,20 @@ print("Diggers; developer; gruszm")
 #BBC
 class ArithmeticDifference(ABC):
     @abstractmethod
-    def difference[T: float](self, a: T, b: T) -> T:
+    def Difference[T: float](self, a: T, b: T) -> T:
         raise NotImplementedError
 
 class ArithmeticsMultiply(ABC):
     @abstractmethod
-    def multiplication[T: float](self, a: T, b: T) -> T:
+    def Multiplication[T: float](self, a: T, b: T) -> T:
         raise NotImplementedError
 # this... is very hard to implement
 class ArithmeticDivision(ABC):
     @abstractmethod
     def division[T: float](self, a: T, b: T) -> T:
         raise NotImplementedError
-#the best abstract class ever ;)
+#the best abstract class ever ;) 
 class ArithmeticsAdd(ABC):
     @abstractmethod
-    def addition[T: float](self, a: T, b: T) -> T:
+    def Addition[T: float](self, a: T, b: T) -> T:
         raise NotImplementedError
